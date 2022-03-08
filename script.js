ad22b43af37f3137e1811f942c33a560116ee230
RossBunnell <rossbunnell@gmail.com>
2022-03-07 20:52:12 -0700

    Added function to check computer vs person to see who wins

--- script.js
+++ script.js
@@ -1,32 +1,44 @@
-/* Generate Computer Choice
-Prompt for user choice (to lowercase)
-
-*/
-
-
-
 //function to generate computer choice
-function computerPlay () {
-    let computerChoice;
-    let calculateChoice = Math.floor(Math.random() * 3);
-    console.log(calculateChoice)
-    if (calculateChoice === 0) {
-        computerChoice = 'Rock';
-    } else if (calculateChoice === 1) {
-        computerChoice = 'Paper';
-    } else {
-        computerChoice = 'Scissors'
-    }
-    return computerChoice
-
+function computerPlay() {
+  // declare an empty variable for the conditional statment to populate
+  let computerChoice;
+  //
+  let calculateChoice = Math.floor(Math.random() * 3);
+  // set computerChoice based on
+  if (calculateChoice === 0) {
+    computerChoice = "rock";
+  } else if (calculateChoice === 1) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";
+  }
+  return computerChoice;
 }
 
-
-
-function playRound (playerSelection, computerSelection, numberOfPlays) {
-
-
+function playRound(playerSelection, computerSelection) {
+  //tie condition
+  if (playerSelection === computerSelection) {
+    return "Tie Game!";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You lose, paper beats rock";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You win, rock beats scissors";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You win, paper beats rock";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You lose, scissors beats paper";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You win, scissors beats paper";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "You lose, rock beats scissors";
+  } else {
+    return "what";
+  }
 }
 
 let computerSelection = computerPlay();
-let playerSelection = prompt('Please enter Rock, Paper, or Scissors').toLowerCase();
+let playerSelection = prompt(
+  "Please enter Rock, Paper, or Scissors"
+).toLowerCase();
+
+console.log(playRound(playerSelection, computerSelection));
