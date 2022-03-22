582cd3cc9f097f6190022c7d197bdb1c6c5293de
RossBunnell <rossbunnell@gmail.com>
2022-03-22 07:30:18 -0600

    Added buttons, refactoring code to work with interactive UI

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function computerPlay() {
 };
 
 function playRound(playerSelection, computerSelection) {
+
     if (playerSelection === computerSelection) {
         return `You picked ${playerSelection}, computer picked ${computerSelection}, it's a tie!`;
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
@@ -18,11 +19,30 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
+
+function game(playerchoice) {
     let computerSelection = '';
-    let playerSelection = prompt('Choose Paper, Rock or Scissors').trim().toLowerCase();
+    let playerSelection = playerchoice
     computerSelection = computerPlay().trim().toLowerCase();
     let result = playRound(playerSelection, computerSelection);
+    console.log(result);
     return result;
 
 }
+
+// let playerSelection = '';
+
+let rock = document.querySelector('#rock');
+rock.addEventListener('click', function() {
+    return game('rock');
+})
+
+let paper = document.querySelector('#paper');
+paper.addEventListener('click', function() {
+    return game('paper');
+})
+
+let scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', function() {
+    return game('scissors');
+})
