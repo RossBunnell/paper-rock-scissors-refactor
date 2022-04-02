e35de5bcfa694964f05b304020a0c7bf4b734462
RossBunnell <rossbunnell@gmail.com>
2022-04-01 22:14:50 -0600

    completed javascript game works git push git@github.com:RossBunnell/paper-rock-scissors-refactor.git

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const computerScore = 0;
-const playerScore = 0;
+let computerScore = 0;
+let playerScore = 0;
 const choices = ["paper", "rock", "scissors"];
 let player;
 let computer;
+let resultContainer = document.getElementById('current-game-results');
+let myScore = document.getElementById('player-score');
+let pcScore = document.getElementById('computer-score')
 
 /* Computer Selection */
 
@@ -37,45 +40,37 @@ scissors.addEventListener("click", function() {
 /* Playing a round */
 
 function game(player, computer) {
-    let resultContainer = document.getElementById('current-game-results');
-    resultContainer.textContent = player + ' ' + computer;
+    if (player === computer) {
+        resultContainer.textContent = `You both picked ${player}!   It's a tie!`
+    } else if (computer === 'rock' && player === 'scissors' || computer === 'scissors' && player === 'paper' || computer === 'paper' && player === 'rock') {
+        computerScore++
+        pcScore.textContent = computerScore;
+        resultContainer.textContent = `You picked ${player} and the computer picked ${computer}, you lose!`
+        if (computerScore === 5) {
+            alert("GAME OVER - The Computer Won!")
+            computerScore = 0;
+            pcScore.textContent = computerScore;
+            playerScore = 0;
+            myScore.textContent = playerScore;
+            resultContainer.textContent = 'Choose Rock, Paper, Or Scissors!'
+        } else {
+            return;
+        }
+    } else {
+        playerScore++
+        myScore.textContent = playerScore;
+        resultContainer.textContent = `You picked ${player} and the computer picked ${computer}, you win!`
+        if (playerScore === 5) {
+            alert("GAME OVER - You Won!")
+            playerScore = 0;
+            myScore.textContent = playerScore;
+            computerScore = 0;
+            pcScore.textContent = computerScore;
+            resultContainer.textContent = 'Choose Rock, Paper, Or Scissors!'
+        } else {
+            return;
+        }
+
+    }
+
 }
-
-
-
-// const computerScore = 0;
-// const playerScore = 0;
-
-// /* Computer Selection */
-
-// const computerChoice = function() {
-//     const choices = ['paper', 'rock', 'scissors'];
-//     const randomChoice = Math.floor(Math.random() * 3);
-//     return choices[randomChoice];
-// }
-
-// /* Player Selection */
-
-// const rock = document.querySelector('#rock');
-// rock.addEventListener('click', function rockChoice() {
-//     return game('rock');
-// });
-
-// const paper = document.querySelector('#paper');
-// paper.addEventListener('click', function paperChoice() {
-//     return game('paper');
-// });
-
-// const scissors = document.querySelector('#scissors');
-// scissors.addEventListener('click', function scissorsChoice() {
-//     return game('scissors');
-// });
-
-// /* Play Game */
-
-// function game(playerChoice) {
-//     let computer = computerChoice();
-//     if (playerChoice === computer) {
-//         console.log('tie')
-//     }
-// }
